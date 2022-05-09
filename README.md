7f657c040fa76f9c6d682a04d2b9bb678f5aa814
kkmusau <kkmusau@gmail.com>
2022-05-08 19:55:24 -0500

    updated read.me and added local storage to script.js

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # work-day-scheduler
+
+## Task
+
+ The task was to create a simpple calendar application that allows a user to save events for each hour of the the work day by modifying the starter code. The app features dynamically updated HTML and CSS powered by jQuery. The calender features:
+
+- The current day at the top of the calendar;
+- Timeblocks the represent standard business hours;
+- Timeblocks that are color coded to indicate whether it is in the past, present, or future;
+- When timeblocks are clicked and event can be entered;
+- The save button is able to save to the local storage;
+- Finally, once refresed the saved events persist
+
+
+## Links
+
+- [GitHub] (https://github.com/kkmusau/work-day-scheduler)
+- [DeployedPage] (https://kkmusau.github.io/work-day-scheduler/)
+
+## Screenshots
+
+![Work Day Scheduler](https://user-images.githubusercontent.com/101844445/167323522-d8c7aacc-a7eb-48da-bf64-07973dee96c6.png)
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,34 @@
 $('#currentDay').text(moment().format('lll'));
 
-let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 let description = $('.description');
 let currentHour = moment().hour();
 
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < 10; i++) {
     if (currentHour === hours [i]) {
         document.getElementById(i).classList.add('present');
     }
-    if (currentHour < hours[i]) {
+    if (currentHour > hours[i]) {
         document.getElementById(i).classList.add('past');
     }
     if (currentHour < hours[i]) {
         document.getElementById(i).classList.add('future');
     }
+}
+
+
+$('.saveBtn').on('click', function () {
+    let text = $(this).siblings('.input').val();
+    let time = $(this).siblings('.hour').text;
+    
+    localStorage.setItem(time, text);
+    console.log('test');
+    localStorage.setItem(time, JSON.stringify(text));
+});
+
+
+for (var i = 0; i <10; i++) {
+    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
 }
